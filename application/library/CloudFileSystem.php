22a5d8fe8be81ab66dd905a9568d9f5aaca2812c
zhangshunling <zhangshunling@hfjy.com>
2018-07-17 14:07:51 +0800

    七牛上传

--- application/library/CloudFileSystem.php
+++ application/library/CloudFileSystem.php
@@ -1,138 +1,83 @@
 <?php
 require_once(dirname(__FILE__).'/CloudFileSystem/uploader.php');
 
-class CloudFileSystem {
+class CloudFileSystem{
+	private $size = 20971520; #20M
+	private $type = 'jpeg,jpg,gif,png,JPG,GIF,PNG,mp4,mov,3gp,apk';
 
-	private $allowsize = 20971520; #20M
-	private $allowtype = 'jpeg,jpg,gif,png,JPG,GIF,PNG,mp4,mov,3gp,apk';
-	private $upload_field = 'upload_file';
-	private $cloud = array();
-
-	function __construct(){
-
-		try{
-
-			$this->cloud[] = new QiniuFileSystem($qiniu_bucket = 'allfree');
-		}catch(Exception $ex){
-			pushAlert($ex->getMessage());
-		}
-	}
-
-
-	#字符串另存为方式
-	function save($base64_string = '',$remote_file = ''){
-		try{
-			#防止创建2级目录
-			$tmp_remote_file = str_replace('/', '.', $remote_file);
-			$tmpFilename = '/tmp/'.$tmp_remote_file;
-			$ifp = fopen($tmpFilename,"wb"); 
-			$string = @base64_decode(str_pad(strtr($base64_string, '-_', '+/'), strlen($base64_string) % 4, '=', STR_PAD_RIGHT));
-		    fwrite($ifp,$string); 
-		    fclose($ifp);
-
-		    $flag = 1;
-			foreach ($this->cloud as $cloud) {
-				$flag = $cloud->upload($tmpFilename,$remote_file) && $flag;
-			}
-			unlink($tmpFilename);
-			return $flag;
-			
-		}catch(Exception $ex){
-			throw new Exception("error_code:-20057");
-			return false;
-		}
-	}
-	
-	#上传
-	function upload($upload_field = '',$remote_file = ''){
-		
+	#文件上传至七牛
+	function upload_file($upload_field = '',$remote_file = ''){
 		$uploader = new Helper_Uploader();
 		if($uploader->existsFile($upload_field)){
-			$postfile = $uploader->file($upload_field);
-			if(!$postfile->isValid($this->allowtype, $this->allowsize)){
-				throw new Exception("error_code:-20055");
+			$host_file = $uploader->file($upload_field);
+			if(!$host_file->isValid($this->type, $this->size)){
 				return false;
 			}
-
-			#上传文件至本地和远程云存储
-			$flag = 1;
-			foreach ($this->cloud as $cloud) {
-				$flag = $cloud->upload($postfile->tmpFilename(),$remote_file) && $flag;
-			}
-			return $flag;
+			$qiniu = new Qiniu();
+			$qiniu->putFile($remote_file,$host_file->tmpFilename());
+			return $remote_file;
 		}
+		return false;
 	}
 
-	#上传
-	function upload_from_localfile($localfile = '',$remote_file = ''){
-		
+	#视频上传至七牛
+	function upload_video($upload_field = '',$remote_file = ''){
 		$uploader = new Helper_Uploader();
-
-		#上传文件至本地和远程云存储
-		$flag = 1;
-		foreach ($this->cloud as $cloud) {
-			$flag = $cloud->upload($localfile,$remote_file) && $flag;
-		}
-		return $flag;
-	}
-	
-	#删除,cloud_flag = 0 全部云平台, cloud_flag = 1 七牛云平台, cloud_flag = 2 又拍云平台
-	function delete($remote_file,$cloud_flag = 0){
-		$flag = 1;
-		foreach ($this->cloud as $cloud) {
-			if($cloud instanceof QiniuFileSystem && $cloud_flag != 2){
-				$flag = $cloud->delete($remote_file) && $flag;
-			}elseif($cloud instanceof YouPaiFileSystem && $cloud_flag != 1){
-				$flag = $cloud->delete($remote_file) && $flag;
-			}
-		}
-		return $flag;
-	}
-	
-	#上传
-	function upload_local($upload_field = '', $newanme = ''){
-		
-		$newpath = APPLICATION_PATH.$newanme;
-		$this->mkdirs(dirname($newpath));
-		$tmp_name = $_FILES[$upload_field]['tmp_name'];
-		$attach_saved = false;
-		if(@copy($tmp_name, $newpath) || (function_exists('move_uploaded_file') && @move_uploaded_file($tmp_name, $newpath)) || @rename($tmp_name, $newpath)) {
-			@unlink($tmp_name);
-			$attach_saved = true;
-			return true;
-		}
-		if(!$attach_saved && is_readable($tmp_name)) {
-			$fp = @fopen($tmp_name, 'rb');
-			@flock($fp, 2);
-			$attachedfile = @fread($fp, $_FILES[$upload_field]['size']);
-			@fclose($fp);
-
-			$fp = @fopen($newpath, 'wb');
-			@flock($fp, 2);
-			if(@fwrite($fp, $attachedfile)) {
-				@unlink($tmp_name);
-				$attach_saved = true;
-				return true;
-			}
-			@fclose($fp);
-		}
-		if(!$attach_saved) {
-			return false;
-		}
-	}
-	function delete_local($remote_file){
-		@unlink(APPLICATION_PATH.$remote_file);
-		return ;
-	}
-	function mkdirs($dir) {
-		if(!is_dir($dir)) {  
-			if(!$this->mkdirs(dirname($dir))) {  
+		if($uploader->existsFile($upload_field)){
+			$host_file = $uploader->file($upload_field);
+			if(!$host_file->isValid($this->type, $this->size)){
 				return false;
 			}
-			if(!mkdir($dir, 0777)){  
-				return false;  
-			}  
-		}  
-		return true;  
+			$qiniu = new Qiniu();
+			$result = $qiniu->putFile($remote_file,$host_file->tmpFilename(),'video');
+			$return = array();
+			if(isset($result[0]['key'])){
+				$return['link'] = $result[0]['key'];
+				$return['persistentId'] = $result[0]['persistentId'];//进程ID用于回调更新封面图
+			}
+			return $return;
+		}
+		return false;
 	}
+
+	#base64上传至七牛
+	function upload_base64($base64_string = '',$remote_file = ''){
+		$tmp_remote_file = str_replace('/', '.', $remote_file);//防止创建2级目录
+		$tmpFilename = '/tmp/'.$tmp_remote_file;
+		$ifp = fopen($tmpFilename,"wb");
+		$string = @base64_decode(str_pad(strtr($base64_string, '-_', '+/'), strlen($base64_string) % 4, '=', STR_PAD_RIGHT));
+		fwrite($ifp,$string);
+		fclose($ifp);
+		$qiniu = new Qiniu();
+		$qiniu->putFile($remote_file,$tmpFilename);
+		unlink($tmpFilename);
+		return $remote_file;
+	}
+
+	#远程url上传至七牛
+	function upload_fetch($url,$remote_file=''){
+		if(empty($remote_file)){
+			$ext_name = substr(strrchr($url,'.'),1);
+			$remote_file = uniqid(rand(), true) . "." . $ext_name;
+		}
+
+		if($url && $remote_file) {
+			$qiniu = new Qiniu();
+			$result = $qiniu->manager('fetch',$remote_file, $url);
+			return $result;
+		}
+		return false;
+	}
+
+	#删除指定七牛文件
+	function delete($remote_file){
+		if($remote_file) {
+			$qiniu = new Qiniu();
+			$result = $qiniu->manager('delete',$remote_file);
+			return $result;
+		}
+		return false;
+	}
+
+
 }
