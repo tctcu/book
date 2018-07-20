b402586e079d05393d4dd98e0b6c66ed241194f5
zhangshunling <zhangshunling@hfjy.com>
2018-07-20 16:56:33 +0800

    mail

--- application/library/SendMail.php
+++ application/library/SendMail.php
@@ -1,26 +1,22 @@
 <?php
-require_once(dirname(__FILE__).'/PHPMailer/class.phpmailer.php');
+require(dirname(__FILE__).'/PHPMailer/email.class.php');
 
 class SendMail {
 	function send($to, $title, $content) {
-		$mail = new PHPMailer(true); 
-		$mail->IsSMTP(); 
-		$mail->CharSet='UTF-8'; //设置邮件的字符编码，这很重要，不然中文乱码 
-		$mail->SMTPAuth = true; //开启认证 
-		$mail->Port = 25; 
-		$mail->Host = "smtp.qiye.aliyun.com";
-		$mail->Username = "postmaster@wzzsl.com";
-		$mail->Password = "Wzzsl.com";
-		$mail->From = "postmaster@wzzsl.com";
-		$mail->FromName = "管理员";
-		$mail->AddAddress($to); 
-		#$mail->AddAddress($to); 抄送
-		$mail->Subject = $title; 
-		$mail->Body = $content; 
-		$mail->WordWrap = 80; // 设置每行字符串的长度 
-		$mail->IsHTML(true); 
-		$ret = $mail->Send();
-		return $ret;
+		$mailto=$to;
+		$mailsubject=$title;
+		$mailbody=$content;
+		$smtpserver     = "smtp.wzzsl.com";
+		$smtpserverport = 25;
+		$smtpusermail   = "postmaster@wzzsl.com";
+		$smtpuser       = "postmaster@wzzsl.com";
+		$smtppass       = "Wzzsl.com";
+		$mailsubject    = "=?UTF-8?B?" . base64_encode($mailsubject) . "?=";
+		$mailtype       = "HTML";
+		$smtp           = new smtp($smtpserver, $smtpserverport, true, $smtpuser, $smtppass);
+		//var_dump($smtp);die;
+		$smtp->debug    = false;
+		return $smtp->sendmail($mailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
 	}
 	
 }
